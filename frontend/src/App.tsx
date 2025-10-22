2c7514c1f012402012f18a138e06fef2db9ad505
ericbenson34652-dotcom <eric.benson34652@gmail.com>
2025-10-23 00:51:59 +0500

    Update App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AIPriceEstimator from "./pages/AIPriceEstimator";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <AIPriceEstimator />
+    </div>
+  );
+}
+
+export default App;
